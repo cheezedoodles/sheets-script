096fdf6eab44ec539bea98bcb88ad36fc8f666f8
alex <hougvapes@gmail.com>
2022-10-01 20:01:02 +0300

    fixed a bug where it couldn't delete a row from db because of len(sheets)

--- sheets_script.py
+++ sheets_script.py
@@ -14,30 +14,32 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-SAMPLE_SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
-SAMPLE_RANGE_NAME = 'A2:D'
+SAMPLE_SPREADSHEET_ID = os.environ["SPREADSHEET_ID"]
+SAMPLE_RANGE_NAME = "A2:D"
 
-DB_NAME = os.environ['DB_NAME']
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_HOST = os.environ['DB_HOST']
-DB_PORT = os.environ['DB_PORT']
+DB_NAME = os.environ["DB_NAME"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
 
 
 @lru_cache()
-def get_currency_rates(ttl_hash=None): # basically memoization with 4 hours duration
-    """Returns xml-like dict of today's rates from cbr.ru"""
-    current_date = datetime.today().date().strftime('%d/%m/%Y')
+def get_currency_rates(ttl_hash=None):  # basically memoization with 4 hours duration
+    """
+    Returns xml-like dict of today's rates from cbr.ru
+    """
+    current_date = datetime.today().date().strftime("%d/%m/%Y")
     response = requests.get(
-        f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}'
-        )
+        f"https://www.cbr.ru/scripts/XML_daily.asp?date_req={current_date}"
+    )
     currencies = xmltodict.parse(response.content)
     return currencies
 
 
-def get_ttl_hash(seconds=14400): # 4 hours by default
+def get_ttl_hash(seconds=14400):  # 4 hours by default
     """
     Return the same value withing `seconds` time period
     """
@@ -46,9 +48,9 @@ def get_ttl_hash(seconds=14400): # 4 hours by default
 
 def convert_to_rub(price_usd, currencies):
     price_rub = None
-    for i in currencies['ValCurs']['Valute']:
-        if i['@ID'] == 'R01235':
-            price_rub = Decimal(i['Value'].replace(',', '.')) * price_usd
+    for i in currencies["ValCurs"]["Valute"]:
+        if i["@ID"] == "R01235":
+            price_rub = Decimal(i["Value"].replace(",", ".")) * price_usd
     return price_rub
 
 
@@ -56,37 +58,42 @@ def main():
 
     creds = None
 
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        service = build("sheets", "v4", credentials=creds)
 
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
 
         if not values:
-            print('No data found.')
+            print("No data found.")
             return
         try:
-            conn = psycopg2.connect(database=DB_NAME,
-                                        user=DB_USER,
-                                        password=DB_PASSWORD,
-                                        host=DB_HOST,
-                                        port=DB_PORT)
+            conn = psycopg2.connect(
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT,
+            )
             cur = conn.cursor()
-            cur.execute("""
+            cur.execute(
+                """
         
                   CREATE TABLE IF NOT EXISTS orders(
                   id serial PRIMARY KEY NOT NULL,
@@ -96,19 +103,21 @@ def main():
                   price_rub numeric(15,6) NOT NULL,
                   UNIQUE(order_number)
                   );
-        """)
+        """
+            )
             conn.commit()
             conn.close()
             print("Database initialized successfully")
         except:
             print("Database did not initialize successfully")
         while True:
-            #try:
-            conn = psycopg2.connect(database=DB_NAME,
-                                    user=DB_USER,
-                                    password=DB_PASSWORD,
-                                    host=DB_HOST,
-                                    port=DB_PORT)
+            conn = psycopg2.connect(
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT,
+            )
             cur = conn.cursor()
 
             cur.execute("SELECT id FROM orders;")
@@ -117,59 +126,80 @@ def main():
             cur.execute("SELECT count(*) FROM orders;")
             id_count = cur.fetchone()[0]
 
-            print(existing_ids, id_count)
+            blank_id = 0
             for row in values:
                 try:
                     id = int(row[0])
                     order_number = int(row[1])
                     price_usd = Decimal(row[2])
-                    delivery_date = datetime.strptime(row[3], '%d.%m.%Y').date()
+                    delivery_date = datetime.strptime(row[3], "%d.%m.%Y").date()
+                    currencies = get_currency_rates(ttl_hash=get_ttl_hash())
+                    price_rub = convert_to_rub(
+                        price_usd=price_usd, currencies=currencies
+                    )
                 except:
-                    print('this field is blank or deleted')
+                    blank_id += 1
+
+                    cur.execute(
+                        """
+
+                        DELETE FROM orders WHERE id = %s;
+
+                    """,
+                        (blank_id,),
+                    )
                     continue
 
-                currencies = get_currency_rates(ttl_hash=get_ttl_hash())
-                price_rub = convert_to_rub(price_usd=price_usd, currencies=currencies)
-                
-                print(id, order_number, price_usd, delivery_date, price_rub)
+                blank_id = id
                 s_id = (id,)
+
                 if s_id not in existing_ids:
                     if id > id_count:
-                        cur.execute("""
+                        cur.execute(
+                            """
 
                             INSERT INTO orders (id, order_number, price_usd, delivery_date, price_rub)
                             VALUES (%s, %s, %s, %s, %s);
 
-                        """, (id, order_number, price_usd, delivery_date, price_rub)
-                        )
-                    else:
-                        cur.execute("""
-
-                            DELETE FROM orders WHERE id = %s;
-
-                        """, (id)
+                        """,
+                            (
+                                id,
+                                order_number,
+                                price_usd,
+                                delivery_date,
+                                price_rub,
+                            ),
                         )
                 else:
-                    cur.execute("""
+                    cur.execute(
+                        """
 
                             UPDATE orders 
                             SET order_number = %s, price_usd = %s, delivery_date = %s, price_rub = %s
                             WHERE id = %s;
 
-                        """, (order_number, price_usd, delivery_date, price_rub, id)
-                        )
+                        """,
+                        (
+                            order_number,
+                            price_usd,
+                            delivery_date,
+                            price_rub,
+                            id,
+                        ),
+                    )
             conn.commit()
             conn.close()
 
             time.sleep(30)
-            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-            values = result.get('values', [])
-            # except:
-            #     print("Something went wrong while sheets_script was running")
+            result = (
+                sheet.values()
+                .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+                .execute()
+            )
+            values = result.get("values", [])
     except HttpError as err:
         print(err)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
